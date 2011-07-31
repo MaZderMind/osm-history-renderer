cfed98698587ca538d37dc28f2c806511be483d8
Peter <github@mazdermind.de>
2011-07-31 17:02:14 +0200

    experimental postgres scheme

---
+++ scheme/00-before.sql
@@ -0,0 +1,102 @@
+-- requires hstore_new, postgis, gist_btree
+
+START TRANSACTION;
+
+DROP TABLE IF EXISTS hist_points;
+CREATE TABLE hist_points (
+    -- optional 64bit support
+    -- id BIGINT,
+    id int,
+    version smallint,
+    valid_from timestamp without time zone,
+    valid_to timestamp without time zone,
+    tags hstore
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_points',
+
+    -- column name
+    'geom',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type
+    'POINT',
+
+    -- dimensions
+    2
+);
+
+
+DROP TABLE IF EXISTS hist_lines;
+CREATE TABLE hist_lines (
+    -- optional 64bit support
+    -- id BIGINT,
+    id int,
+    version smallint,
+    valid_from timestamp without time zone,
+    valid_to timestamp without time zone,
+    tags hstore
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_lines',
+
+    -- column name
+    'geom',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type
+    'LINESTRING',
+
+    -- dimensions
+    2
+);
+
+
+DROP TABLE IF EXISTS hist_areas;
+CREATE TABLE hist_areas (
+    -- optional 64bit support
+    -- id BIGINT,
+    id int,
+    version smallint,
+    valid_from timestamp without time zone,
+    valid_to timestamp without time zone,
+    tags hstore
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_areas',
+
+    -- column name
+    'geom',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type
+    'MULTIPOLYGON',
+
+    -- dimensions
+    2
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_areas',
+
+    -- column name
+    'center',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type
+    'POINT',
+
+    -- dimensions
+    2
+);
