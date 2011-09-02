216d0c29c398ee6a356873131d6bade618f6f3b7
Peter Körner <github@mazdermind.de>
2011-09-02 12:34:31 +0200

    adjust testsuite to produce equal-width images

--- renderer/testsuite.sh
+++ renderer/testsuite.sh
@@ -2,21 +2,21 @@
 DATE=`date +'%Y-%m-%d %H:%M:%S'`
 STYLE=styles/osmorg/osmorg.xml
 
-./render.py --style $STYLE --date $DATE --zoom 1 --file 01
-./render.py --style $STYLE --date $DATE --zoom 2 --file 02
-./render.py --style $STYLE --date $DATE --bbox -100,-10,100,70 --zoom 3 --file 03
-./render.py --style $STYLE --date $DATE --bbox -17.14,56.37,27.61,35.34 --zoom 4 --file 04
-./render.py --style $STYLE --date $DATE --bbox -17.14,56.37,27.61,35.34 --zoom 5 --file 05
-./render.py --style $STYLE --date $DATE --bbox 5.56,55.04,15.35,47.15 --zoom 6 --file 06
-./render.py --style $STYLE --date $DATE --bbox 5.56,55.04,15.35,47.15 --zoom 7 --file 07
-./render.py --style $STYLE --date $DATE --bbox 7.24,51.04,10.21,49.5 --zoom 8 --file 08
-./render.py --style $STYLE --date $DATE --bbox 7.24,51.04,10.21,49.5 --zoom 9 --file 09
-./render.py --style $STYLE --date $DATE --bbox 7.552,49.525,8.834,50.028 --zoom 10 --file 10
-./render.py --style $STYLE --date $DATE --bbox 7.971,49.651,8.415,49.903 --zoom 11 --file 11
-./render.py --style $STYLE --date $DATE --bbox 8.0818,49.7141,8.3042,49.8398 --zoom 12 --file 12
-./render.py --style $STYLE --date $DATE --bbox 8.1374,49.7456,8.2486,49.8084 --zoom 13 --file 13
-./render.py --style $STYLE --date $DATE --bbox 8.1508,49.7655,8.2308,49.7969 --zoom 14 --file 14
-./render.py --style $STYLE --date $DATE --bbox 8.1769,49.77334,8.2047,49.78906 --zoom 15 --file 15
-./render.py --style $STYLE --date $DATE --bbox 8.18389,49.77981,8.1978,49.78766 --zoom 16 --file 16
-./render.py --style $STYLE --date $DATE --bbox 8.18898,49.78206,8.19593,49.78598 --zoom 17 --file 17
-./render.py --style $STYLE --date $DATE --bbox 8.191165,49.783138,8.194641,49.785102 --zoom 18 --file 18
+./render.py --style $STYLE --date $DATE --bbox -180,-85,180,85 --zoom 1 --file 01
+./render.py --style $STYLE --date $DATE --bbox -180,-70,202,85 --zoom 2 --file 02
+./render.py --style $STYLE --date $DATE --bbox -88.5,-20.5,104.9,79.4 --zoom 3 --file 03
+./render.py --style $STYLE --date $DATE --bbox -40.1,18,56.5,69.1 --zoom 4 --file 04
+./render.py --style $STYLE --date $DATE --bbox -16,35.4,32.4,60.9 --zoom 5 --file 05
+./render.py --style $STYLE --date $DATE --bbox -3.89,43.02,20.28,55.72 --zoom 6 --file 06
+./render.py --style $STYLE --date $DATE --bbox 2.15,46.51,14.24,52.85 --zoom 7 --file 07
+./render.py --style $STYLE --date $DATE --bbox 5.17,48.17,11.21,51.34 --zoom 8 --file 08
+./render.py --style $STYLE --date $DATE --bbox 6.682,48.985,9.703,50.57 --zoom 9 --file 09
+./render.py --style $STYLE --date $DATE --bbox 7.438,49.386,8.948,50.179 --zoom 10 --file 10
+./render.py --style $STYLE --date $DATE --bbox 7.815,49.586,8.571,49.982 --zoom 11 --file 11
+./render.py --style $STYLE --date $DATE --bbox 8.004,49.685,8.3817,49.8832 --zoom 12 --file 12
+./render.py --style $STYLE --date $DATE --bbox 8.0984,49.7347,8.2873,49.8337 --zoom 13 --file 13
+./render.py --style $STYLE --date $DATE --bbox 8.1457,49.7594,8.2401,49.809 --zoom 14 --file 14
+./render.py --style $STYLE --date $DATE --bbox 8.16926,49.77183,8.21646,49.7966 --zoom 15 --file 15
+./render.py --style $STYLE --date $DATE --bbox 8.18106,49.77803,8.20466,49.79041 --zoom 16 --file 16
+./render.py --style $STYLE --date $DATE --bbox 8.18696,49.78112,8.19876,49.78732 --zoom 17 --file 17
+./render.py --style $STYLE --date $DATE --bbox 8.18991,49.782672,8.19581,49.785769 --zoom 18 --file 18
