b365053d9f36544657bcd3b676fa6be5c426d40b
MaZderMind <github@mazdermind.de>
2011-12-11 22:23:04 +0100

    document db connection classes

--- importer/dbconn.hpp
+++ importer/dbconn.hpp
@@ -1,52 +1,93 @@
 /**
- * The importer populates a postgres-database. It uses COPY streams to
- * pipe data into the server. This class controls a pipe into the
- * database.
+ * The importer populates a postgres-database. This class controls a
+ * connection to the database.
  */
 
 #ifndef IMPORTER_DBCONN_HPP
 #define IMPORTER_DBCONN_HPP
 
+/**
+ * Controls a connection to the database
+ */
 class DbConn {
 protected:
+    /**
+     * Postgres API handle
+     */
     PGconn *conn;
 
 public:
+    /**
+     * Create a new, unconnected controller
+     */
     DbConn() : conn(NULL) {}
 
+    /**
+     * Delete the controller and disconnect
+     */
+    ~DbConn() {
+        close();
+    }
+
+    /**
+     * Connect the controller to a database specified by the dsn
+     */
     void open(const std::string& dsn) {
+        // connect to the database
         conn = PQconnectdb(dsn.c_str());
 
+        // check, that the connection status is OK
         if(PQstatus(conn) != CONNECTION_OK)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQfinish(conn);
             throw std::runtime_error("connection to database failed");
         }
 
+        // disable sync-commits
+        //  see http://lists.openstreetmap.org/pipermail/dev/2011-December/023854.html
         PGresult *res = PQexec(conn, "SET synchronous_commit TO off;");
+
+        // check, that the query succeeded
         if(PQresultStatus(res) != PGRES_COMMAND_OK)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQclear(res);
             PQfinish(conn);
             throw std::runtime_error("setting synchronous_commit to off failed");
         }
+
+        PQclear(res);
     }
 
+    /**
+     * Close the connection with the database
+     */
     void close() {
+        // but only if there is a opened connection
+        if(!conn) return;
+
+        // close the connection
         PQfinish(conn);
+
+        // nullify the connection handle
         conn = NULL;
     }
 
-    void execfile(std::ifstream& f) {
-        std::string cmd((std::istreambuf_iterator<char>(f)),
-                         std::istreambuf_iterator<char>());
-
+    /**
+     * execute one or more sql statements
+     */
+    void exec(const std::string& cmd) {
+        // execute the command
         PGresult *res = PQexec(conn, cmd.c_str());
+
+        // check, that the query succeeded
         ExecStatusType status = PQresultStatus(res);
         if(status != PGRES_COMMAND_OK && status != PGRES_TUPLES_OK)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQresultErrorMessage(res) << std::endl;
             PQclear(res);
             PQfinish(conn);
@@ -54,6 +95,18 @@ public:
         }
 
         PQclear(res);
+    }
+
+    /**
+     * read a .sql-file and execute it
+     */
+    void execfile(std::ifstream& f) {
+        // read the file
+        std::string cmd((std::istreambuf_iterator<char>(f)),
+                         std::istreambuf_iterator<char>());
+
+        // and execute it
+        exec(cmd);
     }
 };
 
--- importer/dbcopyconn.hpp
+++ importer/dbcopyconn.hpp
@@ -1,6 +1,6 @@
 /**
  * The importer populates a postgres-database. It uses COPY streams to
- * pipe data into the server. This class controls a pipe into the
+ * pipe data into the server. This class controls a COPY pipe into the
  * database.
  */
 
@@ -9,103 +9,176 @@
 
 #include "dbconn.hpp"
 
+/**
+ * Controls a COPY pipe into the database.
+ */
 class DbCopyConn : DbConn {
-protected:
-    std::string m_prefix;
-
 public:
+    /**
+     * Create a new, unconnected COPY pipe controller
+     */
     DbCopyConn() : DbConn() {}
 
-    void open(const std::string& dsn, const std::string& prefix, const std::string& table) {
-        DbConn::open(dsn);
-        m_prefix = prefix;
+    /**
+     * Delete the controller, rollback the copied data and disconnect
+     * the copy-pipe controller
+     */
+    ~DbCopyConn() {
+        DbConn::close();
+    }
 
+    /**
+     * Connect the controller to a database specified by the dsn, start
+     * a (fast) transaction and open the COPY pipe to the table specified
+     * by prefix and table
+     */
+    void open(const std::string& dsn, const std::string& prefix, const std::string& table) {
+        // connect to the database
+        DbConn::open(dsn);
+
+        // sql commands are assembled into this stringstream
         std::stringstream cmd;
+
+        // query results are stored in this result pointer
         PGresult *res;
 
-        res = PQexec(conn, "START TRANSACTION;");
+        // try to start a transaction
+        res = PQexec(conn, "BEGIN;");
+
+        // check, that the query succeeded
         if(PQresultStatus(res) != PGRES_COMMAND_OK)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQclear(res);
             PQfinish(conn);
             throw std::runtime_error("starting transaction failed");
         }
 
-        cmd << "TRUNCATE TABLE " << m_prefix << table << ";";
+        // issue a TRUNCATE statement
+        //   this tells postgres that it can throw away the new data in
+        //   case of an error which in turn disables the WriteAheadLog
+        //   for this transaction. See 14.2.2 in the postgres-docs:
+        //   http://www.postgresql.org/docs/9.1/static/populate.html
+        cmd << "TRUNCATE TABLE " << prefix << table << ";";
+
+        // try to truncate the table
         res = PQexec(conn, cmd.str().c_str());
+
+        // check, that the query succeeded
         if(PQresultStatus(res) != PGRES_COMMAND_OK)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQclear(res);
             PQfinish(conn);
-            throw std::runtime_error("starting transaction failed");
+            throw std::runtime_error("truncating table failed");
         }
 
+        // clear the command buffer and the result
+        PQclear(res);
         cmd.str().clear();
 
-        cmd << "COPY " << m_prefix << table << " FROM STDIN;";
+        // assemble the COPY command
+        cmd << "COPY " << prefix << table << " FROM STDIN;";
+
+        // try to start the copy mode
         res = PQexec(conn, cmd.str().c_str());
+
+        // check, that the query succeeded
         if(PQresultStatus(res) != PGRES_COPY_IN)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQresultErrorMessage(res) << std::endl;
-
             PQclear(res);
             PQfinish(conn);
             throw std::runtime_error("COPY FROM STDIN command failed");
         }
 
+        // clear result
         PQclear(res);
     }
 
+    /**
+     * Finish the COPY pipe, commit the transaction and close the
+     * connection to the database
+     */
     void close() {
+        // but only if there is a opened connection
+        if(!conn) return;
+
+        // finish the COPY pipe
         int cpres = PQputCopyEnd(conn, NULL);
+
+        // check, that the copying succeeded
         if(-1 == cpres)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQfinish(conn);
             throw std::runtime_error("COPY FROM STDIN finilization failed");
         }
 
+        // query results are stored in this result pointer
         PGresult *res;
 
+        // get the result of the COPY FROM STDIN command (ansynchronus)
         res = PQgetResult(conn);
-        while(res != NULL) {
-            ExecStatusType status = PQresultStatus(res);
-            switch(status) {
-                case PGRES_FATAL_ERROR:
-                case PGRES_NONFATAL_ERROR:
-                    std::cerr << "error from postgres: " << PQresultErrorMessage(res) << std::endl;
-                    break;
 
+        // check if we do have a result
+        while(res != NULL) {
+            // get restult status and print messages accordingly
+            ExecStatusType status = PQresultStatus(res);
+            std::cerr << "PQresultStatus=" << status << std::endl;
+            switch(status) {
                 case PGRES_COMMAND_OK:
                     break;
 
+                case PGRES_FATAL_ERROR:
+                case PGRES_NONFATAL_ERROR:
+                    std::cerr << "PQresultErrorMessage=" << PQresultErrorMessage(res) << std::endl;
+
                 default:
                     std::cerr << "PQresultStatus=" << status << std::endl;
-                    break;
+                    PQclear(res);
+                    PQfinish(conn);
+                    throw std::runtime_error("COPY FROM STDIN finilization failed");
             }
 
+            // get the next result
             PQclear(res);
-            break;
+            res = PQgetResult(conn);
         }
 
+        // try to commit the open transaction
         res = PQexec(conn, "COMMIT;");
+
+        // check, that the query succeeded
         if(PQresultStatus(res) != PGRES_COMMAND_OK)
         {
+            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQclear(res);
             PQfinish(conn);
             throw std::runtime_error("comitting transaction failed");
         }
 
+        PQclear(res);
+
+        // close the connection to the database
         DbConn::close();
     }
 
+    /**
+     * copy a chunk of data into the COPY pipe
+     */
     void copy(const std::string& data) {
+        // copy data into the pipe
         int res = PQputCopyData(conn, data.c_str(), data.size());
 
+        // check if the copying succeeded
         if(-1 == res) {
+            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQfinish(conn);
             throw std::runtime_error("COPY data-transfer failed");
