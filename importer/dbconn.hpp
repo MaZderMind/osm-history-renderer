0e19ccd414529dae171ce4b11096f9c91dc8a98e
Rares Aioanei <rares.aioanei@telenav.com>
2017-09-26 17:09:32 +0300

    First iteration of implementing logging

--- importer/dbconn.hpp
+++ importer/dbconn.hpp
@@ -7,7 +7,9 @@
 #define IMPORTER_DBCONN_HPP
 
 #include <libpq-fe.h>
+#include "logger.hpp"
 
+extern void init();
 /**
  * Controls a connection to the database
  */
@@ -30,7 +32,24 @@ public:
     ~DbConn() {
         close();
     }
-
+    //Specify a notice receiver/processor for non-fatal psql errors to be used here and in dbcopyconn.hpp
+    static void noticerecv(void *arg, const PGresult *res)
+    {
+        void init();
+        logging::add_common_attributes();
+        using namespace logging::trivial;
+        src::severity_logger< severity_level > lg;
+        if (res != NULL)
+        {
+            printf("%s\n", 
+                PQresultErrorField(res,
+                    PG_DIAG_MESSAGE_PRIMARY));
+            //Write to logger as well
+            BOOST_LOG_SEV(lg, warning) << PQresultErrorField(res, PG_DIAG_MESSAGE_PRIMARY); //FIXME : Show other relevant fields here, add log verbosity flag to the main app
+                                                                                            //and display more details only if requested
+        }
+	}
+    
     /**
      * Connect the controller to a database specified by the dsn
      */
@@ -39,7 +58,7 @@ public:
         conn = PQconnectdb(dsn.c_str());
 
         // check, that the connection status is OK
-        if(PQstatus(conn) != CONNECTION_OK)
+        if(PQstatus(conn) == CONNECTION_BAD)
         {
             // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
@@ -51,14 +70,23 @@ public:
         //  see http://lists.openstreetmap.org/pipermail/dev/2011-December/023854.html
         PGresult *res = PQexec(conn, "SET synchronous_commit TO off;");
         // check that the query succeeded
-        if(PQresultStatus(res) != PGRES_COMMAND_OK)
+		PQsetNoticeReceiver(conn, noticerecv, NULL);	
+        if(PQresultStatus(res) == PGRES_FATAL_ERROR)
         {
-            // show the error message, close the connection and throw out
             std::cerr << PQerrorMessage(conn) << std::endl;
             PQclear(res);
             PQfinish(conn);
             throw std::runtime_error("Setting synchronous_commit to off failed");
         }
+		else if(PQresultStatus(res) == PGRES_BAD_RESPONSE)
+		{
+			printf("%s: error: %s\n",
+                PQresStatus(PQresultStatus(res)),
+                PQresultErrorMessage(res));
+			PQclear(res);
+			PQfinish(conn);
+			throw std::runtime_error("Bad response from server.");
+		}	
 
         PQclear(res);
     }
@@ -87,7 +115,6 @@ public:
         ExecStatusType status = PQresultStatus(res);
         if(status != PGRES_COMMAND_OK && status != PGRES_TUPLES_OK)
         {
-            // show the error message, close the connection and throw out
             std::cerr << PQresultErrorMessage(res) << std::endl;
             PQclear(res);
             PQfinish(conn);
